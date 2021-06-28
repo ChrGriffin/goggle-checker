e66ae756954ce7058401fc9dbb2a75480262e961
Christian Griffin <christian.griffin@live.com>
2021-06-28 18:09:19 -0400

    Fix indenting.

--- GoggleCheck.lua
+++ GoggleCheck.lua
@@ -11,33 +11,33 @@ local function tableHasValue(theTable, search)
 end
 
 local function isGoggleArea(areaName)
-	return tableHasValue({'Nagrand', 'Shadowmoon Valley', 'Netherstorm', 'Zangarmarsh'}, areaName);
+    return tableHasValue({'Nagrand', 'Shadowmoon Valley', 'Netherstorm', 'Zangarmarsh'}, areaName);
 end
 
 local function isNonGoggleArea(areaName)
-	return IsInInstance();
+    return IsInInstance();
 end
 
 local function gogglesAreEquipped()
-	return IsEquippedItem('Ultra-Spectropic Detection Goggles');
+    return IsEquippedItem('Ultra-Spectropic Detection Goggles');
 end
 
 frame:SetScript('OnEvent', function(_, event)
 
-	local currentArea = GetRealZoneText();
+    local currentArea = GetRealZoneText();
     if isGoggleArea(currentArea) then
 
-    	if not gogglesAreEquipped() then
-    		print('Equip Goggles!');
-    	end
+        if not gogglesAreEquipped() then
+            print('Equip Goggles!');
+        end
 
-	elseif isNonGoggleArea(currentArea) then
+    elseif isNonGoggleArea(currentArea) then
 
-    	if gogglesAreEquipped() then
-    		print('Unequip Goggles!');
-    	end
+        if gogglesAreEquipped() then
+            print('Unequip Goggles!');
+        end
 
-	end
+    end
 end)
 
 frame:RegisterEvent('ZONE_CHANGED_NEW_AREA');
